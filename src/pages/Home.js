73d1ceb16280a3af6d72814b814e50b4c6d50863
Alireza9476 <bagheria96@yahoo.com>
2022-10-21 15:00:27 +0200

    homepage, todo: nav responsive

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,9 @@ import HeaderMenu from "../components/HeaderMenu";
 import Standard from "./Standard";
 import { useLocation } from "react-router-dom";
 import Popup from "../components/Popup";
+import styled, { keyframes } from "styled-components";
+
+import { default as Typing } from "../assets/Homepage/Typing.jpg";
 
 function Home() {
   const location = useLocation();
@@ -28,11 +31,48 @@ function Home() {
 
   return (
     <>
-      <div className="mt-20">Home</div>
+      <div className="mt-40"></div>
       {location.state ? <Popup openProp={true} /> : null}
-      {/* <HeaderMenu /> */}
+      <main className="max-w-[1400px] mx-auto">
+        <div className="sm:flex sm:flex-col md:flex md:flex-row justify-between px-12 gap-10 gap-y-10">
+          <div className="max-w-[600px] min-w-[250px]">
+            <h1 className="text-3xl font-bold w-full uppercase">
+              Improve your typing skills with
+            </h1>
+            <h1 className="text-3xl font-bold w-full">
+              Keyboard
+              <RotateI>I</RotateI>
+              <RotateO>O</RotateO>
+            </h1>
+          </div>
+          <div className="max-w-[600px]">
+            <img
+              className="w-full rounded-t-3xl shadow-lg"
+              src={Typing}
+              alt="typing"
+            />
+          </div>
+        </div>
+      </main>
     </>
   );
 }
+
+const rotate = keyframes`
+0%,60% {
+  transform: rotateY(360deg) 
+}
+`;
+
+const RotateI = styled.div`
+  display: inline-block;
+  animation: ${rotate} 2s infinite;
+  animation-delay: calc(0.2s * 1);
+`;
+const RotateO = styled.div`
+  display: inline-block;
+  animation: ${rotate} 2s infinite;
+  animation-delay: calc(0.2s * 2);
+`;
 
 export default Home;
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,8 @@ module.exports = {
         },
       },
       screens: {
-        sm: "300px",
+        // sm: "300px",
+        sxm: "300px",
       },
     },
   },
